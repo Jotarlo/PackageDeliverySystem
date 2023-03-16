38e81d142390d7b3394ab44da73f4b8b680576a1
Jeferson Arango López <jeferson.arango.lopez@outlook.com>
2023-03-16 12:40:43 -0500

    Agregar archivos de proyecto.

---
+++ PackageDelivery.Repository.Contracts/DbModels/Parameters/PersonDbModel.cs
@@ -0,0 +1,16 @@
+﻿namespace PackageDelivery.Repository.Contracts.DbModels.Parameters
+{
+    public class PersonDbModel
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string OtherNames { get; set; }
+        public string FirstLastname { get; set; }
+        public string SecondLastname { get; set; }
+        public string IdentificationNumber { get; set; }
+        public string cellphone { get; set; }
+        public string email { get; set; }
+        public int IdentificationType { get; set; }
+
+    }
+}
